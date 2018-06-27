4ac21b931fd6347b8e00b328cba57c04d5d80b06
= <alla_88@inbox.lv>
2018-06-27 20:10:56 -0300

    tabs are done

--- tabs/js/App.js
+++ tabs/js/App.js
@@ -1,1 +1,19 @@
-
+const activeClassName='tabs__item-active'
+const App = ()=>  (
+    <Router>
+        <div className="tabs">
+            <nav className="tabs__items">
+                <NavLink className="tabs__item" to="/" exact activeClassName={activeClassName}>Рефераты</NavLink>
+                <NavLink className="tabs__item" to="/creator" activeClassName={activeClassName}>Криэйтор</NavLink>
+                <NavLink className="tabs__item" to="/fortune" activeClassName={activeClassName}>Гадалка</NavLink>
+            </nav>
+            <div className="tabs__content">
+                <Switch>
+                    <Route path="/fortune" component={Fortune}/>
+                    <Route path="/creator" component={Creator}/>
+                    <Route path="/" component={Essay}/>
+                </Switch>
+            </div>
+        </div>
+    </Router>
+)
